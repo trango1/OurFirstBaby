a86673c06fe5a0593700e3ff1eb280f5c7c02b6f
Joseph <aelphaeis@live.com>
2015-03-29 02:32:58 -0400

    Message from daddy 2

--- src/driver/Program.java
+++ src/driver/Program.java
@@ -4,5 +4,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hey, I am Joseph and Trang's Baby");
 		System.out.println("This is a message from daddy");
+		System.out.println("We can program together and work on cool things together like this");
 	}
 }
